8c8df078bdb6b3d806948321c17d1fc0578fdbbe
mkiaraujo <mki.araujo@gmail.com>
2024-12-26 15:52:27 -0300

    Desafio mapeando relacionamentos muitos para muitos

---
+++ src/main/java/com/algaworks/algafood/domain/model/Usuario.java
@@ -0,0 +1,40 @@
+package com.algaworks.algafood.domain.model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Usuario {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @CreationTimestamp
+    @Column(name = "data_cadastro",nullable = false, columnDefinition = "datetime")
+    private LocalDateTime dataCadastro;
+
+    @ManyToMany
+    @JoinTable(name = "usuario_grupo", joinColumns = @JoinColumn(name = "usuario_id"),
+            inverseJoinColumns = @JoinColumn(name = "grupo_id"))
+    private List<Grupo> grupos = new ArrayList<>();
+}
