5339361582c9117f3d022c4de73ab53059396ee8
mkiaraujo <mki.araujo@gmail.com>
2024-12-26 13:12:34 -0300

    Mapeando relacionamento muitos para um

--- src/main/java/com/algaworks/algafood/domain/model/Produto.java
+++ src/main/java/com/algaworks/algafood/domain/model/Produto.java
@@ -1,2 +1,34 @@
-package com.algaworks.algafood.domain.model;public class Produto {
+package com.algaworks.algafood.domain.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.math.BigDecimal;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Produto {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    private String descricao;
+
+    @Column(nullable = false)
+    private BigDecimal preco;
+
+    @Column(nullable = false)
+    private Boolean ativo;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurante_id", nullable = false)
+    private Restaurante restaurante;
+
 }
