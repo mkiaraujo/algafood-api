2326f2f5d0855e871076862e2cdeaf00210aa40b
mkiaraujo <mki.araujo@gmail.com>
2025-01-02 14:19:27 -0300

    Customizando exception handler de ResponseEntityExceptionHandler

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -14,6 +15,19 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex ,
+            HttpHeaders headers , HttpStatusCode status , WebRequest request) {
+
+        ProblemType problemType = ProblemType.MENSAGEM_INCOMPREENSIVEL;
+        String detail = "O corpo da requisição está inválido. Verifique erro de sintaxe.";
+
+        Problem problem = createProblemBuilder((HttpStatus) status, problemType, detail).build();
+
+        return handleExceptionInternal(ex, problem, new HttpHeaders(),
+                status, request);
+    }
 
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
     public ResponseEntity<?> handlerEntidadeNaoEncontradaException(EntidadeNaoEncontradaException ex,
@@ -23,13 +37,6 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         String detail = ex.getMessage();
 
         Problem problem = createProblemBuilder(status, problemType, detail).build();
-
-//        Problem problem = Problem.builder()
-//                .status(status.value())
-//                .type("https://algafood.com.br/entidade-nao-encontrada")
-//                .title("Entidade não encontrada")
-//                .detail(ex.getMessage())
-//                .build();
 
         return handleExceptionInternal(ex, problem, new HttpHeaders(),
                 status, request);
