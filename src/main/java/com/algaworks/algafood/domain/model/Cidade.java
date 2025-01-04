c09cded8eba02dbe47095943a468cfffa581a6a7
mkiaraujo <mki.araujo@gmail.com>
2025-01-04 18:38:13 -0300

    Desafio adicionando constraints de validação no modelo

--- src/main/java/com/algaworks/algafood/domain/model/Cidade.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cidade.java
@@ -1,8 +1,14 @@
 package com.algaworks.algafood.domain.model;
 
 
+import com.algaworks.algafood.Groups;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.groups.ConvertGroup;
+import jakarta.validation.groups.Default;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -16,10 +22,13 @@ public class Cidade {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     @Column(nullable = false)
     private String nome;
 
-//    @JsonIgnore
+    @Valid
+    @ConvertGroup(from = Default.class, to = Groups.EstadoId.class)
+    @NotNull
     @ManyToOne
     @JoinColumn(nullable = false)
     private Estado estado;
