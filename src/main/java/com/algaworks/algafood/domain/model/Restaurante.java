640938d62d0a836f3960dd26c9b6ead1c9bd1fec
mkiaraujo <mki.araujo@gmail.com>
2025-01-05 15:08:15 -0300

    Criando constraints de validação customizadas usando composição

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.domain.model;
 
 import com.algaworks.algafood.core.validation.Groups;
+import com.algaworks.algafood.core.validation.TaxaFrete;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.Valid;
@@ -32,7 +33,8 @@ public class Restaurante {
     private String nome;
 
     @NotNull
-    @PositiveOrZero//(message = "{TaxaFrete.invalida}")
+//    @PositiveOrZero
+    @TaxaFrete
     @Column(name = "taxa_frete", nullable = false)
     private BigDecimal taxaFrete;
 
