f6f720147136289919fdbd031c2b328f296c5845
mkiaraujo <mki.araujo@gmail.com>
2025-01-06 18:07:02 -0300

    Criando e rodando um teste de integração com SpringBoot JUnit e AssertJ

---
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIntegrationTests.java
@@ -0,0 +1,41 @@
+package com.algaworks.algafood;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.service.CadastroCozinhaService;
+import jakarta.validation.ConstraintViolationException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class CadastroCozinhaIntegrationTests {
+
+    @Autowired
+    private CadastroCozinhaService cadastroCozinha;
+
+    @Test
+    public void testarCadastroCozinhaComSucesso() {
+//        cenário
+        Cozinha novaCozinha = new Cozinha();
+        novaCozinha.setNome("Chinesa");
+//        ação
+        novaCozinha = cadastroCozinha.salvar(novaCozinha);
+//        validação
+        Assertions.assertThat(novaCozinha).isNotNull();
+        Assertions.assertThat(novaCozinha.getId()).isNotNull();
+    }
+
+    @Test//(expected = ConstraintViolationException.class)
+    public void testarCadastroCozinhaSemNome() {
+        final Cozinha novaCozinha = new Cozinha();
+        novaCozinha.setNome(null);
+
+        Assertions.assertThatThrownBy(() -> cadastroCozinha.salvar(novaCozinha)).isInstanceOf(ConstraintViolationException.class);
+//        novaCozinha = cadastroCozinha.salvar(novaCozinha);
+
+
+    }
+
+}
