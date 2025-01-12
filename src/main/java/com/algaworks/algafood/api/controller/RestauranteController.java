d56264eac5697c4c820a537335a8fc710ab6677e
mkiaraujo <mki.araujo@gmail.com>
2025-01-12 19:10:01 -0300

    Refatorando e criando um assembler de DTO

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.api.controller;
 
+import com.algaworks.algafood.api.assembler.RestauranteModelAssembler;
 import com.algaworks.algafood.api.model.CozinhaModel;
 import com.algaworks.algafood.api.model.RestauranteModel;
 import com.algaworks.algafood.api.model.input.CozinhaIdInput;
@@ -44,16 +45,20 @@ public class RestauranteController {
     @Autowired
     private SmartValidator smartValidator;
 
+    @Autowired
+    private RestauranteModelAssembler restauranteModelAssembler;
+
     @GetMapping
     public List<RestauranteModel> listar() {
-        return toCollectionModel(restauranteRepository.findAll());
+
+        return restauranteModelAssembler.toCollectionModel(restauranteRepository.findAll());
     }
 
     @GetMapping("/{restauranteId}")
     public RestauranteModel buscar(@PathVariable Long restauranteId) {
         Restaurante restaurante = cadastroRestaurante.buscarOuFalhar(restauranteId);
 
-        return toModel(restaurante);
+        return restauranteModelAssembler.toModel(restaurante);
     }
 
     @PostMapping
@@ -63,7 +68,7 @@ public class RestauranteController {
         try {
             Restaurante restaurante = toDomainObject(restauranteInput);
 
-            return toModel(cadastroRestaurante.salvar(restaurante));
+            return restauranteModelAssembler.toModel(cadastroRestaurante.salvar(restaurante));
         } catch (EntidadeNaoEncontradaException e){
             throw new NegocioException(e.getMessage());
         }
@@ -77,7 +82,7 @@ public class RestauranteController {
             Restaurante restauranteAtual = cadastroRestaurante.buscarOuFalhar(restauranteId);
             BeanUtils.copyProperties(restaurante, restauranteAtual,
                         "id", "formasPagamento", "endereco", "dataCadastro", "produtos");
-            return toModel(cadastroRestaurante.salvar(restauranteAtual));
+            return restauranteModelAssembler.toModel(cadastroRestaurante.salvar(restauranteAtual));
         } catch (EntidadeNaoEncontradaException e){
             throw new NegocioException(e.getMessage());
         }
@@ -98,17 +103,7 @@ public class RestauranteController {
         return atualizar(restauranteId, restauranteInput);
     }
 
-    private RestauranteInput toInputModel(Restaurante restauranteAtual) {
-        RestauranteInput restauranteInput = new RestauranteInput();
-        restauranteInput.setNome(restauranteAtual.getNome());
-        restauranteInput.setTaxaFrete(restauranteAtual.getTaxaFrete());
 
-        CozinhaIdInput cozinhaIdInput = new CozinhaIdInput();
-        cozinhaIdInput.setId(restauranteAtual.getCozinha().getId());
-
-        restauranteInput.setCozinha(cozinhaIdInput);
-        return restauranteInput;
-    }
 
     private void validate(Restaurante restaurante, String objectName) {
         BeanPropertyBindingResult bindingResult = new BeanPropertyBindingResult(restaurante, objectName);
@@ -152,26 +147,6 @@ public class RestauranteController {
         cadastroRestaurante.excluir(restauranteId);
     }
 
-
-    private static RestauranteModel toModel(Restaurante restaurante) {
-        CozinhaModel cozinhaModel = new CozinhaModel();
-        cozinhaModel.setId(restaurante.getCozinha().getId());
-        cozinhaModel.setNome(restaurante.getCozinha().getNome());
-
-        RestauranteModel restauranteModel = new RestauranteModel();
-        restauranteModel.setId(restaurante.getId());
-        restauranteModel.setNome(restaurante.getNome());
-        restauranteModel.setTaxaFrete(restaurante.getTaxaFrete());
-        restauranteModel.setCozinha(cozinhaModel);
-        return restauranteModel;
-    }
-
-    private List<RestauranteModel> toCollectionModel(List<Restaurante> restaurantes){
-        return restaurantes.stream()
-                .map(restaurante -> toModel(restaurante))
-                .collect(Collectors.toList());
-    }
-
     private Restaurante toDomainObject(RestauranteInput restauranteInput) {
         Restaurante restaurante = new Restaurante();
         restaurante.setNome(restauranteInput.getNome());
@@ -183,5 +158,17 @@ public class RestauranteController {
         restaurante.setCozinha(cozinha);
 
         return restaurante;
+    }
+
+    private RestauranteInput toInputModel(Restaurante restauranteAtual) {
+        RestauranteInput restauranteInput = new RestauranteInput();
+        restauranteInput.setNome(restauranteAtual.getNome());
+        restauranteInput.setTaxaFrete(restauranteAtual.getTaxaFrete());
+
+        CozinhaIdInput cozinhaIdInput = new CozinhaIdInput();
+        cozinhaIdInput.setId(restauranteAtual.getCozinha().getId());
+
+        restauranteInput.setCozinha(cozinhaIdInput);
+        return restauranteInput;
     }
 }
--- src/main/java/com/algaworks/algafood/core/jackson/JacksonMixinModule.java
+++ src/main/java/com/algaworks/algafood/core/jackson/JacksonMixinModule.java
@@ -3,7 +3,6 @@ package com.algaworks.algafood.core.jackson;
 import com.algaworks.algafood.api.model.mixin.CidadeMixin;
 import com.algaworks.algafood.api.model.mixin.CozinhaMixin;
 import com.algaworks.algafood.api.model.mixin.EnderecoMixin;
-import com.algaworks.algafood.api.model.mixin.RestauranteMixin;
 import com.algaworks.algafood.domain.model.Cidade;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Endereco;
@@ -15,7 +14,6 @@ import org.springframework.stereotype.Component;
 public class JacksonMixinModule extends SimpleModule {
 
     public JacksonMixinModule() {
-        setMixInAnnotation(Restaurante.class, RestauranteMixin.class);
         setMixInAnnotation(Cidade.class, CidadeMixin.class);
         setMixInAnnotation(Cozinha.class, CozinhaMixin.class);
         setMixInAnnotation(Endereco.class, EnderecoMixin.class);
