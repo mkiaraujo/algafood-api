e3b710fba4d57cc030880180f8ad714b87ac4804
mkiaraujo <mki.araujo@gmail.com>
2025-02-16 18:50:07 -0300

    Foi implementada entidade FotoProduto

---
+++ src/main/java/com/algaworks/algafood/domain/model/FotoProduto.java
@@ -0,0 +1,30 @@
+package com.algaworks.algafood.domain.model;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class FotoProduto {
+
+
+    @EqualsAndHashCode.Include
+    @Id
+    @Column(name = "produto_id")
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    private Produto produto;
+
+    private String nomeArquivo;
+    private String descricao;
+    private String contentType;
+    private Long tamanho;
+
+}
