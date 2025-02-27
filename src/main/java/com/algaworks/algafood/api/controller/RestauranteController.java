3c774ae35f32bf9187bf784094fecc6649be64e5
mkiaraujo <mki.araujo@gmail.com>
2025-02-27 00:26:50 -0300

    Entendendo o funcionamento de preflight do CORS

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -32,7 +32,7 @@ import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin
+@CrossOrigin(maxAge = 10)
 @RestController
 @RequestMapping("/restaurantes")
 public class RestauranteController {
