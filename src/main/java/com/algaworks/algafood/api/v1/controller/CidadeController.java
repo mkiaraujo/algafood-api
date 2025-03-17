2dfbeed345479270d89bf4b34b3e81cddba43a27
mkiaraujo <mki.araujo@gmail.com>
2025-03-16 22:44:21 -0300

    Implementando o versionamento da API por URI

--- src/main/java/com/algaworks/algafood/api/v1/controller/CidadeController.java
+++ src/main/java/com/algaworks/algafood/api/v1/controller/CidadeController.java
@@ -5,7 +5,6 @@ import com.algaworks.algafood.api.v1.assembler.CidadeInputDisassembler;
 import com.algaworks.algafood.api.v1.assembler.CidadeModelAssembler;
 import com.algaworks.algafood.api.v1.model.CidadeModel;
 import com.algaworks.algafood.api.v1.model.input.CidadeInput;
-import com.algaworks.algafood.core.web.AlgaMediaTypes;
 import com.algaworks.algafood.domain.exception.EstadoNaoEncontradoException;
 import com.algaworks.algafood.domain.exception.NegocioException;
 import com.algaworks.algafood.domain.model.Cidade;
@@ -19,7 +18,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(path = "/cidades", produces = AlgaMediaTypes.V1_APPLICATION_JSON_VALUE)
+@RequestMapping(path = "/v1/cidades", produces = MediaType.APPLICATION_JSON_VALUE)
 public class CidadeController {
 
     @SuppressWarnings("unused")
