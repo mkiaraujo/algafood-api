dad8c2fd671d4b5b4b7f829c92feed18b50ab4aa
mkiaraujo <mki.araujo@gmail.com>
2025-03-17 23:20:54 -0300

    Configurando spring security com HTTP Basic

--- src/main/java/com/algaworks/algafood/core/security/WebSecurityConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/WebSecurityConfig.java
@@ -2,8 +2,10 @@ package com.algaworks.algafood.core.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,12 +17,17 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/public/**").permitAll() // Permite acesso público
-                        .requestMatchers("/v1/**").authenticated() // Exige autenticação para /api/**
+                                .requestMatchers( "/v1/cozinhas/**").permitAll()
+                                .anyRequest()
+                                .authenticated() // Exige autenticação para /api/**
                 )
                 .httpBasic(httpBasic -> {}) // Habilita autenticação HTTP Basic
                 .csrf(csrf -> csrf.disable()); // Desabilita CSRF para APIs RESTful
+
 
         return http.build();
 
