578fca31a075a42b4987e1286a99b1b10902420f
mkiaraujo <mki.araujo@gmail.com>
2025-03-24 12:29:31 -0300

    Configuração do o resource server com a nova stack do spring security

--- src/main/java/com/algaworks/algafood/core/security/WebSecurityConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/WebSecurityConfig.java
@@ -2,17 +2,8 @@ package com.algaworks.algafood.core.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,45 +11,17 @@ import org.springframework.security.web.SecurityFilterChain;
 public class WebSecurityConfig {
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user1 = User.builder()
-                .username("thiago")
-                .password(passwordEncoder().encode("123")) // Codifica a senha
-                .roles("ADMIN")
-                .build();
-
-        UserDetails user2 = User.builder()
-                .username("joao")
-                .password(passwordEncoder().encode("123")) // Codifica a senha
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(user1, user2);
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(); // Usa BCrypt para codificar senhas
-    }
-
-
-    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
                 .authorizeHttpRequests(authorize -> authorize
-                                .requestMatchers( "/v1/cozinhas/**").permitAll()
-                                .anyRequest()
-                                .authenticated() // Exige autenticação para /api/**
+//                                .requestMatchers( "/v1/cozinhas/**").permitAll()
+                                .anyRequest().authenticated() // Exige autenticação para /api/**
                 )
-                .httpBasic(httpBasic -> {}) // Habilita autenticação HTTP Basic
-                .csrf(csrf -> csrf.disable()); // Desabilita CSRF para APIs RESTful
-
+                .oauth2ResourceServer(oauth2 -> oauth2.opaqueToken());
 
         return http.build();
 
     }
+
 }
