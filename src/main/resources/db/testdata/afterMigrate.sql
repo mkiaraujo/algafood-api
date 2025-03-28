93d993e2d6a3c5e5e8aa0fbfca7d4c5dbf4fd7cb
mkiaraujo <mki.araujo@gmail.com>
2025-03-28 00:50:31 -0300

    Escalando a aplicação - executando 2 containers

--- src/main/resources/db/testdata/afterMigrate.sql
+++ src/main/resources/db/testdata/afterMigrate.sql
@@ -1,5 +1,9 @@
 set foreign_key_checks = 0;
 
+lock tables cidade write, cozinha write, estado write, forma_pagamento write, grupo write, grupo_permissao write, permissao write,
+ produto write, restaurante write, restaurante_forma_pagamento write, usuario write, usuario_grupo write, restaurante_usuario_responsavel write,
+ pedido write, item_pedido write, foto_produto write;
+
 delete from cidade;
 delete from cozinha;
 delete from estado;
@@ -140,3 +144,6 @@ values (5, '8d774bcf-b238-42f3-aef1-5fb388754d63', 1, 3, 2, 1, '38400-200', 'Rua
 
 insert into item_pedido (id, pedido_id, produto_id, quantidade, preco_unitario, preco_total, observacao)
 values (6, 5, 3, 1, 87.2, 87.2, null);
+
+
+unlock tables;
