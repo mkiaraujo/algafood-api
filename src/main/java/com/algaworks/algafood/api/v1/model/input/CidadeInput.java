96e44c98a058d66e065b31c15eeece2167a49694
mkiaraujo <mki.araujo@gmail.com>
2025-03-31 12:07:39 -0300

    Descrevendo modelos de representações e suas propriedades

--- src/main/java/com/algaworks/algafood/api/v1/model/input/CidadeInput.java
+++ src/main/java/com/algaworks/algafood/api/v1/model/input/CidadeInput.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.api.v1.model.input;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -10,6 +11,7 @@ import lombok.Setter;
 @Setter
 public class CidadeInput {
 
+    @Schema(example = "Uberlândia")
     @NotBlank
     private String nome;
 
