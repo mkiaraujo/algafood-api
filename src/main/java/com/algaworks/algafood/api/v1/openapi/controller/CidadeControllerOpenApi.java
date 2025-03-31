64baaee9ee565a6e78b2e269009f5e0ec9256e92
mkiaraujo <mki.araujo@gmail.com>
2025-03-31 11:43:31 -0300

    Descrevendo parâmetros de entrada nos métodos

--- src/main/java/com/algaworks/algafood/api/v1/openapi/controller/CidadeControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v1/openapi/controller/CidadeControllerOpenApi.java
@@ -3,6 +3,8 @@ package com.algaworks.algafood.api.v1.openapi.controller;
 import com.algaworks.algafood.api.v1.model.CidadeModel;
 import com.algaworks.algafood.api.v1.model.input.CidadeInput;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.hateoas.CollectionModel;
@@ -16,16 +18,22 @@ public interface CidadeControllerOpenApi {
     CollectionModel<CidadeModel> listar();
 
     @Operation(summary = "Busca uma cidade por id", description = "Busca uma cidade informando um id válido")
-    CidadeModel buscar(Long cidadeId);
+    CidadeModel buscar(@Parameter(description = "ID de uma cidade", example = "1", required = true) Long cidadeId);
 
-    @Operation(summary = "Cadastra uma cidade", description = "Cadastro de uma cidade necessita de um estado e um nome válido")
-    CidadeModel adicionar(CidadeInput cidadeInput);
+    @Operation(summary = "Cadastra uma cidade", description = "Cadastro de uma cidade necessita de um estado " +
+            "e um nome válido")
+    CidadeModel adicionar(
+            @RequestBody(description = "Representação de uma nova cidade", required = true) CidadeInput cidadeInput);
 
     @Operation(summary = "Atualiza uma cidade", description = "Atualiza uma cidade informando seu id " +
             " e um objeto contendo novo nome da cidade e um estdo com seu id válido")
-    CidadeModel atualizar(Long cidadeId, CidadeInput cidadeInput);
+    CidadeModel atualizar(
+            @Parameter(description = "ID de uma cidade", example = "1", required = true) Long cidadeId,
+            @RequestBody(description = "Representação de uma cidade com dados para atualizar", required = true)
+            CidadeInput cidadeInput);
 
     @Operation(summary = "Remove uma cidade pelo id", description = "Remove uma cidade informando um id válido")
-    ResponseEntity<Void> remover(Long cidadeId);
+    ResponseEntity<Void> remover(
+            @Parameter(description = "ID de uma cidade", example = "1", required = true) Long cidadeId);
 
 }
